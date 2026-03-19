0757f087304fb4fda0cad68c3bcb4be5cbfddea9
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-03-19 23:23:43 +0530

    got all basic info input for validation

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 // 1. take all input filed
 newQuizBtn = document.querySelector('#new_quiz');
 quizAddBlock = document.querySelector('.quiz_builder_block');
+basicInputTitle = document.querySelector('#title');
+addQunsBtn = document.querySelector('#add_quns');
+basicInputCatogery = document.querySelector('#categorey');
+basicInputTime = document.querySelector('#time_limit');
+basicInputDesc = document.querySelector('#description');
 
+
+
+newQuizBtn.style.display = 'flex';
+quizAddBlock.style.display = 'block';
+infoValidation()
 
 // 2.add events
 newQuizBtn.addEventListener('click',() => {
     quizAddBlock.style.display = 'block';
     newQuizBtn.style.display = 'none'
+    
 
 })
-
-
- newQuizBtn.style.display = 'flex'
-
 
 
 
 // 3. functions
 
 
+function infoValidation() {
+    addQunsBtn.addEventListener('click',()=>{
+        let inputTitle = basicInputTitle.value;
+        let inputCategoery = basicInputCatogery.value;
+        let inputTime = basicInputTime.value;
+        let inputDesc = basicInputDesc.value;
+// console.log(inputDesc,'inputDesc');
+ })
 
+}
