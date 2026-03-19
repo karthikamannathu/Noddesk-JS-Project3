5563a234cc85e934b36c9b5aadb4183d855f2cfe
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-03-19 23:00:03 +0530

    quiz add button click event is wrk

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // 1. take all input filed
 newQuizBtn = document.querySelector('#new_quiz');
-
+quizAddBlock = document.querySelector('.quiz_builder_block');
 
 
 // 2.add events
+newQuizBtn.addEventListener('click',() => {
+    quizAddBlock.style.display = 'block';
+    newQuizBtn.style.display = 'none'
 
-newQuizBtn
+})
 
+
+ newQuizBtn.style.display = 'flex'
 
 
 
